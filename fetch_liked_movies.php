9c6dd598ba571321619bf697feb16a90472830f2
iden seo <epik35@naver.com>
2024-11-13 19:46:33 +0900

    마이페이지 완료

--- fetch_liked_movies.php
+++ fetch_liked_movies.php
@@ -4,8 +4,6 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json; charset=utf-8');
 
-include 'db_connection.php'; // 데이터베이스 연결 파일
-
 // 데이터베이스 연결 설정
 $servername = "210.117.212.110";
 $username = "iden";
@@ -21,24 +19,29 @@ if ($conn->connect_error) {
 
 // POST 요청에서 userId를 가져옵니다.
 $data = json_decode(file_get_contents("php://input"));
-$userId = $data->userId; // 클라이언트에서 전달된 userId
+$userId = $data->userId;
 
 // 좋아요한 영화 정보를 가져오는 쿼리
 $query = "SELECT m.id, m.title, m.release_date, m.vote_average, m.poster_path, m.overview
-          FROM likes l
-          JOIN movies m ON l.movieId = m.id
+          FROM movies m
+          JOIN likes l ON m.id = l.movieId
           WHERE l.userId = ?";
+
 $stmt = $conn->prepare($query);
 $stmt->bind_param("i", $userId);
 $stmt->execute();
 $result = $stmt->get_result();
 
 $movies = array();
-while ($row = $result->fetch_assoc()) {
-    $movies[] = $row;
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $movies[] = $row;
+    }
+    echo json_encode($movies);
+} else {
+    echo json_encode(["success" => false, "message" => "No movies found for this user."]);
 }
 
-echo json_encode($movies);
 $stmt->close(); // prepared statement 닫기
 $conn->close(); // 데이터베이스 연결 닫기
 ?>
